be67ef578e9debe4424bda49af6953906fafd50c
Reinhard Fink @ Home <reinhard.fink@tsn.at>
2021-01-03 13:52:58 +0100

    add class folders to createNamedPDF.sh

--- createNamedPDF.sh
+++ createNamedPDF.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ${1}=="" ];
+if [ "${1}" == "" ];
 then
         echo "Usage ${0} Filename"
         exit 1
@@ -10,6 +10,7 @@ ZEUGNISART="Notenmitteilung"
 
 mkdir pages
 
+# split many pages in one document into numberd single pages
 pdftk ${1} burst output pages/Seite_%03d.pdf 
 
 cd pages
@@ -17,14 +18,26 @@ cd pages
 for pdffile in $(ls *pdf);
 do
         echo ""
-        echo "Convert ${pdffile} to Textfile"
         textfile=${pdffile%pdf}txt
+        echo "Convert PDF to Textfile"
         echo "pdftotext ${pdffile} ${textfile}"
         pdftotext ${pdffile} ${textfile}
+
+        echo "Get class from textfile"
+        class=$(grep "Klasse:" ${textfile} | awk '{print $2}')
+        if [ ! -d class ];
+        then
+                mkdir ${class}
+        fi
+
         echo "Get pupils name from textfile"
+        # grep -A 1 print one line after matching line
         name=$(grep -A 1 ${ZEUGNISART} ${textfile} | tail -1 | sed -e {'s/ /_/g'})
-        echo ${name}
-        mv -v ${pdffile} ${name}.pdf
+
+        echo "Move numberd PDF-file to named PDF-file in class directory"
+        echo "mv -v ${pdffile} ${class}/${name}.pdf"
+        mv -v ${pdffile} ${class}/${name}.pdf
+        echo "remove Textfile"
         rm -v ${textfile}
 done
 
